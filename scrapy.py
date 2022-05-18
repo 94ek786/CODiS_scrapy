96a89cb4218dfad692d06d96cd741e12e4a1b54a
94ek786 <e0970633123@gmail.com>
2022-05-18 17:04:21 +0800

    2

--- scrapy.py
+++ scrapy.py
@@ -3,31 +3,51 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import time
+import json
 
-loop_city = 
-loop_station = 0
+time_range = [[2022, 5], [2020, 1]]
+M_T = ['00','01','02','03','04','05','06','07','08','09','10','11','12']
+
+def is_float(value):
+  try:
+    float(value)
+    return True
+  except:
+    return False
+
+def sc(i):
+    loop_time = time_range[0]
+    while(loop_time != time_range[1]):
+        browser.get('https://e-service.cwb.gov.tw/HistoryDataQuery/MonthDataController.do?command=viewMain&station=' + str(i[0]) + '&stname=%25E9%259E%258D%25E9%2583%25A8&datepicker=' + str(loop_time[0]) + '-' + M_T[loop_time[1]] + '&altitude=0m')
+        time.sleep(1)
+        if(is_float(browser.find_element_by_xpath('//*[@id="MyTable"]/tbody/tr[4]/td[8]').text)):
+            print(loop_time)
+        else:
+            txt = i[0] + i[1] + ' stop at ' + str(loop_time)
+            return txt
+        if(loop_time[1] == 1):
+            loop_time[0] -= 1
+            loop_time[1] = 12
+        else:
+            loop_time[1] -= 1
+
+    txt = i[0] + i[1] + ' good'
+    return txt
 browser = webdriver.Chrome(executable_path='C:\\driver\\chromedriver.exe')
-browser.get('https://e-service.cwb.gov.tw/HistoryDataQuery/')
-time.sleep(3)
-select_city = Select(browser.find_element_by_id('stationCounty'))
-city_len = len(select_city.options)
 
-while(loop_city < city_len):
-    browser.get('https://e-service.cwb.gov.tw/HistoryDataQuery/')
-    time.sleep(3)
-    #選擇縣市
-    select = Select(browser.find_element_by_id('stationCounty'))
-    select.select_by_index(loop_city)
-    #選擇月報表
-    select = Select(browser.find_element_by_id('datatype'))
-    select.select_by_index(1)
-    #選日期
-    browser.find_element_by_id('datepicker').send_keys('2022-05')
-    browser.find_element_by_id('doquery').click()
-    time.sleep(10)
-    loop_city += 1
-    print(loop_city)
-    
+#讀取station_id.json
+with open('station_id.json','r') as jsonfile:
+    stationId = json.load(jsonfile)
+jsonfile.close()
+
+s_log = []
+
+for loop in stationId:
+    stxt = sc(loop)
+    s_log.append(stxt)
+    print(stxt)
+print(s_log)
+
 
 time.sleep(100)
 
--- test.py
+++ test.py
@@ -3,11 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import time
+import json
+
+def is_float(value):
+  try:
+    float(value)
+    return True
+  except:
+    return False
+
+a_err = 'https://e-service.cwb.gov.tw/HistoryDataQuery/MonthDataController.do?command=viewMain&station=466921&stname=%25E8%2587%25BA%25E5%258C%2597(%25E5%25B8%25AB%25E9%2599%25A2)&datepicker=2022-05&altitude=6.1m'
+a_yas = 'https://e-service.cwb.gov.tw/HistoryDataQuery/MonthDataController.do?command=viewMain&station=466910&stname=%25E9%259E%258D%25E9%2583%25A8&datepicker=2022-05&altitude=0m'
 
 browser = webdriver.Chrome(executable_path='C:\\driver\\chromedriver.exe')
-browser.get('https://e-service.cwb.gov.tw/HistoryDataQuery/')
+browser.get(a_err)
+time.sleep(2)
+i = browser.find_element_by_xpath('//*[@id="MyTable"]/tbody/tr[4]/td[8]')
+if(is_float(i.text)):
+    print('y')
+else:
+    print('n')
 
-time.sleep(3)
-
-
-time.sleep(100)
+time.sleep(50)
